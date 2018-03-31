30318b979085f6478c49361f124cb5a174608670
Hamza Bentahar <59384@aui.ma>
2018-03-31 13:00:28 +0100

    Correction of equations

--- src/components/course.vue
+++ src/components/course.vue
@@ -1,10 +1,50 @@
 <template>
     <div class="tile is-parent">
+        <modal v-show="showModal" @close="showModal = false">
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">
+                        Change Letter Grades for {{ data.name }}
+                    </p>
+                </header>
+                <div class="card-content">
+                    <div class="content">
+                        <table class="table is-narrow">
+                            <thead>
+                                <tr>
+                                    <th>Letter</th>
+                                    <th>Min</th>
+                                    <th>Max</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr :key="index" v-for="(letterGrade, index) in userLetterGrades">
+                                    <td>{{ letterGrade.letter}}</td>
+                                    <td><input class="input is-small" type="text" v-model="letterGrade.min" v-if="letterGrade.min"></td>
+                                    <td><input class="input is-small" type="text" v-model="letterGrade.max" v-if="letterGrade.max"></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <footer class="card-footer">
+                    <a href="#" class="card-footer-item">Save</a>
+                    <a href="#" class="card-footer-item">Reset</a>
+                </footer>
+            </div>
+        </modal>
         <div class="tile is-child box">
             <div v-if="course" @input="newInput">
-                <div class="field">
-                    <div class="control">
-                        <input class="input" type="text" placeholder="Course Name" v-model="data.name">
+                <div class="columns">
+                    <div class="column is-9">
+                        <div class="field">
+                            <div class="control">
+                                <input class="input" type="text" placeholder="Course Name" v-model="data.name">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <button class="button" @click="showModal = true">Settings</button>
                     </div>
                 </div>
                 <table class="table is-narrow">
@@ -16,7 +56,7 @@
                     </tr>
                     </thead>
                     <tbody>
-                    <tr v-for="assignment in course.assignments">
+                    <tr v-for="(assignment, index) in course.assignments" :key="index">
                         <td>
                             <div class="field">
                                 <div class="control">
@@ -44,7 +84,8 @@
                 </table>
                 <button class="button is-small is-primary" @click="add">Add Assignment</button>
                 <button class="button is-small is-danger" @click="remove">Remove last row</button>
-                <button class="button is-small is-danger" @click="deleteCourse">Remove this course from the list</button>
+                <button class="button is-small is-danger" @click="deleteCourse">Remove this course from the list
+                </button>
                 <hr>
                 <div class="columns">
                     <div class="column">
@@ -55,7 +96,9 @@
                             </div>
                         </div>
                         <p v-if="remaining">
-                            To end up with a grade of {{ remaining }}%, you will need {{ remainingAssignments }}% on the
+                            To end up with a grade of {{ remaining }}% ({{ getLetterGrade(remaining)}}), you will need
+                            <strong>{{ remainingAssignments
+                                }}%</strong> on the
                             remaining assignments.
                         </p>
                     </div>
@@ -67,7 +110,9 @@
                             </div>
                         </div>
                         <p v-if="prediction">
-                            Your final grade is gonna be <strong>{{ finalGrade }}</strong> if you get {{ prediction }}% on
+                            Your final grade is going to be <strong>{{ finalGrade }} ({{
+                            getLetterGrade(finalGrade)}})</strong> if you get {{ prediction }}%
+                            on
                             the remaining assignments
                         </p>
                     </div>
@@ -79,72 +124,96 @@
 
 <script>
   /* eslint-disable */
+  import modal from './modal'
+
   export default {
     name: "course",
+    components: {
+      modal
+    },
     props: {
       data: {
         required: true,
         type: Object
+      },
+      letterGrades: {
+        type: Array
       }
     },
     data() {
       return {
         course: null,
         prediction: null,
-        remaining: null
+        remaining: null,
+        showModal: false,
+        userLetterGrades: []
       }
     },
     computed: {
       finalGrade() {
-        let average = this.average()
-        average += parseFloat(this.prediction) * (100 - this.weight()) / 100
-        return average.toFixed(2)
+        return (((parseFloat(this.prediction) * this.remainingWeight()) / 100) + this.weightedAverage()).toFixed(2)
       },
       remainingAssignments() {
-        return ((this.remaining - this.average()) * 100 / this.weight()).toFixed(2)
+        return (((this.remaining - this.weightedAverage()) * 100) / this.remainingWeight()).toFixed(2)
       }
     },
     mounted() {
       this.course = this.data
+      this.userLetterGrades = this.letterGrades
     },
     methods: {
-      average(){
-        let average = 0
-        for (let assignment in this.course.assignments) {
-          let info = this.course.assignments[assignment]
-          if (info.grade !== '' && info.weight !== '') {
-            average += (parseFloat(info.grade) * parseFloat(info.weight)) / 100
-          }
-        }
-        return average
-      },
-      weight(){
+      weightedAverage() {
         let weight = 0
         for (let assignment in this.course.assignments) {
           let info = this.course.assignments[assignment]
           if (info.grade !== '' && info.weight !== '') {
-            weight += parseFloat(info.weight)
+            weight += parseFloat(info.grade) * (parseFloat(info.weight) / 100)
           }
         }
         return weight
       },
-      add(){
+      remainingWeight() {
+        let result = 0
+        for (let assignment in this.course.assignments) {
+          let info = this.course.assignments[assignment]
+          if (info.grade !== '' && info.weight !== '') {
+            result += parseFloat(info.weight)
+          }
+        }
+        return (100 - result)
+      },
+      add() {
         this.course.assignments.push({
           name: '',
           grade: '',
           weight: ''
         })
       },
-      remove(){
+      remove() {
         this.course.assignments.pop()
       },
-      deleteCourse(){
-        if(confirm("Are you sure that you want to remove the course " + this.course.name + " from the list?")){
+      deleteCourse() {
+        if (confirm("Are you sure that you want to remove the course " + this.course.name + " from the list?")) {
           Event.$emit('delete-course', this.course.id)
         }
       },
-      newInput(){
+      newInput() {
         Event.$emit('new-input', this.course)
+      },
+      getLetterGrade(grade) {
+        let result = ''
+        for (let letterGrade in this.course.letterGrades) {
+          let value = this.course.letterGrades[letterGrade]
+          if ((grade >= value.min) && (grade < value.max)) {
+            result = value.letter
+          }
+          else if (!value.max && grade >= value.min) {
+            result = value.letter
+          } else if (!value.min && grade < value.max) {
+            result = value.letter
+          }
+        }
+        return result
       }
     }
   }
--- src/components/courses.vue
+++ src/components/courses.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container is-fluid">
         <div class="columns is-multiline">
-            <div class="column is-4-desktop is-12-mobile is-6-tablet" v-for="course in courses">
-                <course :data="course"></course>
+            <div class="column is-4-desktop is-12-mobile is-6-tablet" v-for="course in courses" :key="course.id">
+                <course :data="course" :letter-grades="letterGrades"></course>
             </div>
             <div class="column is-4-desktop is-12-mobile is-6-tablet">
                 <div class="tile is-parent">
@@ -26,7 +26,80 @@
     },
     data() {
       return {
-        courses: []
+        courses: [],
+        letterGrades: [
+          {
+            letter: 'A+',
+            min: 97,
+            max: null,
+            gpa: 4.0
+          },
+          {
+            letter: 'A',
+            min: 93,
+            max: 97,
+            gpa: 4.0
+          },
+          {
+            letter: 'A-',
+            min: 90,
+            max: 93,
+            gpa: 3.7
+          },
+          {
+            letter: 'B+',
+            min: 87,
+            max: 90,
+            gpa: 3.3
+          },
+          {
+            letter: 'B',
+            min: 83,
+            max: 87,
+            gpa: 3.0
+          },
+          {
+            letter: 'B-',
+            min: 80,
+            max: 83,
+            gpa: 2.7
+          },
+          {
+            letter: 'C+',
+            min: 77,
+            max: 80,
+            gpa: 2.3
+          },
+          {
+            letter: 'C',
+            min: 73,
+            max: 77,
+            gpa: 2.0
+          },
+          {
+            letter: 'C-',
+            min: 70,
+            max: 73,
+            gpa: 1.7
+          },{
+            letter: 'D+',
+            min: 67,
+            max: 70,
+            gpa: 1.3
+          },
+          {
+            letter: 'D',
+            min: 60,
+            max: 67,
+            gpa: 1.0
+          },
+          {
+            letter: 'F',
+            min: null,
+            max: 60,
+            gpa: 0.0
+          }
+        ]
       }
     },
     mounted(){
@@ -56,6 +129,7 @@
         let course = {
           id: id,
           name: '',
+          credits: null,
           repeat: false,
           assignments: [
             {
@@ -84,62 +158,7 @@
               weight: ''
             }
           ],
-          letterGrades: [
-            {
-              letter: 'A+',
-              min: 97,
-              max: 100
-            },
-            {
-              letter: 'A',
-              min: 93,
-              max: 97
-            },
-            {
-              letter: 'A-',
-              min: 90,
-              max: 93
-            },
-            {
-              letter: 'B+',
-              min: 87,
-              max: 90
-            },
-            {
-              letter: 'B',
-              min: 83,
-              max: 87
-            },
-            {
-              letter: 'B-',
-              min: 80,
-              max: 83
-            },
-            {
-              letter: 'C+',
-              min: 77,
-              max: 80
-            },
-            {
-              letter: 'C',
-              min: 73,
-              max: 77
-            },
-            {
-              letter: 'C-',
-              min: 70,
-              max: 73
-            },{
-              letter: 'D+',
-              min: 67,
-              max: 70
-            },
-            {
-              letter: 'D',
-              min: 60,
-              max: 67
-            }
-          ]
+          letterGrades: this.letterGrades
         }
         this.courses.push(course)
       }
