e62c87a4593721bd4c63a197bc278b4f7999582f
Hamza Bentahar <59384@aui.ma>
2018-04-08 11:56:15 +0100

    Fix google analytics

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueLocalStorage from 'vue-ls'
+import VueAnalytics from 'vue-analytics'
 /* eslint-disable */
 
 require('./assets/sass/main.scss');
 
 Vue.use(VueLocalStorage)
+Vue.use(VueAnalytics, {
+  id: 'UA-117158565-1'
+})
 Vue.config.productionTip = false
 
 Event = new Vue()
