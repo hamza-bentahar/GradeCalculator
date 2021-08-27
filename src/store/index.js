14a2e47f394fadcfaacceff9217366af3cdc4bdf
Hamza Bentahar <59384@aui.ma>
2021-08-27 20:03:19 +0100

    refactor: initialize vuex persistence

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,87 @@
 import Vuex from 'vuex'
 import Vue from "vue"
+import VuexPersistence from 'vuex-persist'
 
-
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 Vue.use(Vuex)
+
 const state = {
-  test: "Hello World"
+  courses: [],
+  letterGrades: [
+    {
+      letter: 'A+',
+      min: 97,
+      max: null,
+      gpa: 4.0
+    },
+    {
+      letter: 'A',
+      min: 93,
+      max: 97,
+      gpa: 4.0
+    },
+    {
+      letter: 'A-',
+      min: 90,
+      max: 93,
+      gpa: 3.7
+    },
+    {
+      letter: 'B+',
+      min: 87,
+      max: 90,
+      gpa: 3.3
+    },
+    {
+      letter: 'B',
+      min: 83,
+      max: 87,
+      gpa: 3.0
+    },
+    {
+      letter: 'B-',
+      min: 80,
+      max: 83,
+      gpa: 2.7
+    },
+    {
+      letter: 'C+',
+      min: 77,
+      max: 80,
+      gpa: 2.3
+    },
+    {
+      letter: 'C',
+      min: 73,
+      max: 77,
+      gpa: 2.0
+    },
+    {
+      letter: 'C-',
+      min: 70,
+      max: 73,
+      gpa: 1.7
+    }, {
+      letter: 'D+',
+      min: 67,
+      max: 70,
+      gpa: 1.3
+    },
+    {
+      letter: 'D',
+      min: 60,
+      max: 67,
+      gpa: 1.0
+    },
+    {
+      letter: 'F',
+      min: null,
+      max: 60,
+      gpa: 0.0
+    }
+  ]
 }
 const getters = {}
 const mutations = {}
@@ -14,7 +91,8 @@ const store = new Vuex.Store({
   state,
   getters,
   mutations,
-  actions
+  actions,
+  plugins: [vuexLocal.plugin]
 })
 
 export default store
