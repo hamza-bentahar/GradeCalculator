8f597de4e6fc0f2f317439e65b134b7bb7bbd499
Hamza Bentahar <59384@aui.ma>
2021-08-27 21:12:29 +0100

    refactor: move GPA calculation panel to its own component

--- src/components/courses.vue
+++ src/components/courses.vue
@@ -16,35 +16,7 @@
             </div>
         </div>
         <div class="column is-one-fifth">
-            <article class="tile notification fixed">
-                <div class="content">
-                    <p class="subtitle">Compute Your GPA</p>
-                    <div class="content">
-                        <div>
-                            <div class="field">
-                                <div class="control">
-                                    <label for="credits">Credits Earned</label>
-                                    <input type="number" class="input" placeholder="Credits Earned" id="credits"
-                                           v-model.number="earnedCredits" min="0">
-                                </div>
-                            </div>
-                            <div class="field">
-                                <div class="control">
-                                    <label for="gpa">Overall GPA</label>
-                                    <input type="number" class="input" placeholder="Overall GPA" id="gpa"
-                                           v-model.number="overallGpa" min="0" max="4">
-                                </div>
-                            </div>
-                        </div>
-                        <hr>
-                        <p class="subtitle">Results</p>
-                        <p>Number of credits for this semester: <strong>{{ getTotalCreditsForSemester }}</strong></p>
-                        <p>Number of credits after this semester: <strong>{{ getTotalCreditsAfterCurrentSemester }}</strong></p>
-                        <p>GPA for this semester: <strong>{{ getCurrentSemesterGPA }}</strong></p>
-                        <p>Overall GPA: <strong>{{ getTotalGPA }}</strong></p>
-                    </div>
-                </div>
-            </article>
+            <gpa-calculation></gpa-calculation>
         </div>
     </div>
 </template>
@@ -52,12 +24,14 @@
 <script>
   /* eslint-disable */
   import Course from './course'
-  import {mapState, mapMutations, mapGetters} from 'vuex'
+  import gpaCalculation from "./gpaCalculation"
+  import {mapState, mapMutations} from 'vuex'
 
   export default {
     name: "courses",
     components: {
-      Course
+      Course,
+      gpaCalculation
     },
     mounted() {
       Event.$on('new-input', data => {
@@ -70,24 +44,6 @@
     },
     computed: {
       ...mapState(['courses', 'letterGrades']),
-      ...mapGetters(['getTotalCreditsForSemester', 'getTotalCreditsAfterCurrentSemester', 'getCurrentSemesterGPA',
-        'getTotalGPA']),
-      earnedCredits: {
-        get () {
-          return this.$store.state.earnedCredits
-        },
-        set (value) {
-          this.$store.commit('updateEarnedCredits', value)
-        }
-      },
-      overallGpa: {
-        get () {
-          return this.$store.state.overallGpa
-        },
-        set (value) {
-          this.$store.commit('updateOverallGPA', value)
-        }
-      }
     },
     methods: {
       ...mapMutations(['addCourse']),
---
+++ src/components/gpaCalculation.vue
@@ -0,0 +1,62 @@
+<template>
+  <article class="tile notification fixed">
+    <div class="content">
+      <p class="subtitle">Compute Your GPA</p>
+      <div class="content">
+        <div>
+          <div class="field">
+            <div class="control">
+              <label for="credits">Credits Earned</label>
+              <input type="number" class="input" placeholder="Credits Earned" id="credits"
+                     v-model.number="earnedCredits" min="0">
+            </div>
+          </div>
+          <div class="field">
+            <div class="control">
+              <label for="gpa">Overall GPA</label>
+              <input type="number" class="input" placeholder="Overall GPA" id="gpa"
+                     v-model.number="overallGpa" min="0" max="4">
+            </div>
+          </div>
+        </div>
+        <hr>
+        <p class="subtitle">Results</p>
+        <p>Number of credits for this semester: <strong>{{ getTotalCreditsForSemester }}</strong></p>
+        <p>Number of credits after this semester: <strong>{{ getTotalCreditsAfterCurrentSemester }}</strong></p>
+        <p>GPA for this semester: <strong>{{ getCurrentSemesterGPA }}</strong></p>
+        <p>Overall GPA: <strong>{{ getTotalGPA }}</strong></p>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+export default {
+  name: "gpaCalculation",
+  computed: {
+    ...mapGetters(['getTotalCreditsForSemester', 'getTotalCreditsAfterCurrentSemester', 'getCurrentSemesterGPA',
+      'getTotalGPA']),
+    earnedCredits: {
+      get () {
+        return this.$store.state.earnedCredits
+      },
+      set (value) {
+        this.$store.commit('updateEarnedCredits', value)
+      }
+    },
+    overallGpa: {
+      get () {
+        return this.$store.state.overallGpa
+      },
+      set (value) {
+        this.$store.commit('updateOverallGPA', value)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
